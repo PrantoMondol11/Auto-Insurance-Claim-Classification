63ac33773d041bb774ef79ab6d88b515e829e178
PrantoMondol11 <mondolpranto83@gmail.com>
2025-12-07 04:25:11 -0500

    Project structure is ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setup import setup,find_packages
+
+setup {
+    name="src",
+    version="0.0.1",
+    author="Pranto Mondol",
+    author_email="mondolpranto83@gmail.com",
+    package=find_packages()
+    }
